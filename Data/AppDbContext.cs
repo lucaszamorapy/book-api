b0a905657af1e4ff6157f72ac1fbaf8679af47d1
lucaszamorapy <lucaszamora.zulmira@gmail.com>
2024-08-12 19:58:48 -0300

    feat: create a database connection and models

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -1,6 +1,16 @@
-﻿namespace WebApi8.Data
+﻿using Microsoft.EntityFrameworkCore;
+using WebApi8.Models;
+
+namespace WebApi8.Data
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext //herdando o dbcontext do entity
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) 
+        {
+            
+        }
+
+        public DbSet<AutorModel> Autores { get; set; }
+        public DbSet<LivroModel> Livros { get; set; }
     }
 }
